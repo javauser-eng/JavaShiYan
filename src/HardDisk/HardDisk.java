185a1f273a695db4d944134e642a775a802c155c
javauser-eng <72697243+javauser-eng@users.noreply.github.com>
2020-10-14 16:13:48 +0800

    Add files via upload

---
+++ src/HardDisk/HardDisk.java
@@ -0,0 +1,16 @@
+package HardDisk;
+
+public class HardDisk {
+	int amount;
+	char model;
+	char brand;
+	public	HardDisk(){
+		
+	}	
+	public int getAmount(){
+		return amount;	
+	}
+	public void setAmount(int amount){
+		this.amount=amount;
+	}
+}
---
+++ src/PC/PC.java
@@ -0,0 +1,20 @@
+
+package PC;
+
+import CPU.CPU;
+import HardDisk.HardDisk;
+
+public class PC {
+	CPU cpu;
+	HardDisk HD;
+	public void setCpu(CPU cpu){
+		this.cpu=cpu;
+	}
+	public void setHardDisk(HardDisk HD){
+		this.HD=HD;
+	}
+	public void show(){
+		System.out.println("CPUspeed:"+cpu.getSpeed());	
+		System.out.println("HDamount:"+HD.getAmount());	
+	}
+}
