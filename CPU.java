46feb2b6a68c98befeaac2cdd6c692ee0791d1e4
javauser-eng <72697243+javauser-eng@users.noreply.github.com>
2020-10-18 00:38:21 +0800

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -1,17 +1,22 @@
 package CPU;
 
 public class CPU {
-	int speed;
-	char model;
-	char brand;
+	private int speed;
+	String model;
+	String brand;
 	public CPU(){
 		
 	}	
 	public int getSpeed(){
 		return speed;
+	}	
+	public String getBrand(){
+		return brand;
 	}
 public void setSpeed(int speed){
 	this.speed=speed;
 	} 
-
+public void setBrand(String brand){
+	this.brand=brand;
+	} 
 }
