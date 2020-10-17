2704b4abce8bbe66dfb5477b53a598c88bae2b5d
javauser-eng <72697243+javauser-eng@users.noreply.github.com>
2020-10-18 00:41:43 +0800

    Update HardDisk.java

--- HardDisk.java
+++ HardDisk.java
@@ -1,16 +1,22 @@
 package HardDisk;
 
 public class HardDisk {
-	int amount;
-	char model;
-	char brand;
+	private int amount;
+	String model;
+	String brand;
 	public	HardDisk(){
 		
 	}	
 	public int getAmount(){
 		return amount;	
 	}
-	public void setAmount(int aount){
-		this.amount=aount;
+	public void setAmount(int amount){
+		this.amount=amount;
+	}
+	public String getBrand(){
+		return brand;	
+	}
+	public void setBrand(String brand){
+		this.brand=brand;
 	}
 }
